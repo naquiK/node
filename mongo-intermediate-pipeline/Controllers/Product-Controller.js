a18e039247949aef6ee77255cfe52675972c9841
naquik <nknaqui72@gmail.com>
2025-06-16 16:20:10 +0530

    $project and $group

--- mongo-intermediate-pipeline/Controllers/Product-Controller.js
+++ mongo-intermediate-pipeline/Controllers/Product-Controller.js
@@ -1,4 +1,98 @@
+const { $where } = require('../../upload-file/models/userModel');
 const Products = require('../Models/Product');
+
+const getProductAnalysis=async (req,res) =>{
+  try {
+    const result = await Products.aggregate([
+      {
+        $match: {
+         category: "Electronics",
+        }
+      },
+      {
+        $group:{
+          _id:null,
+          totalRevenue: {
+            $sum: "$price"
+          },
+          averagePrice: {
+            $avg: "$price"
+          },
+          maxPrice: {
+            $max: "$price"
+          },
+          minPrice: {
+            $min: "$price"
+          },
+        }
+      },
+      {
+        $project: {
+          _id: 0,
+          totalRevenue: 1,
+          averagePrice: 1,
+          maxPrice: 1,
+          minPrice: 1,
+          priceRange: {
+            $subtract: ["$maxPrice", "$minPrice"]
+          }
+        }
+      }
+    ])
+
+    res.status(200).json({
+      success: true,
+      data: result
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message
+    });
+    
+  }
+}
+
+
+const getAllProducts = async (req, res) => {
+  try {
+    
+    const result = await Products.aggregate([
+      {
+        $match:{
+          inStock: true,
+          // price:{$gte:100}
+        },
+      },
+        {
+          $group: {
+            _id: "$category",
+            avgPrice:{
+              $avg: "$price"
+            },
+            count:{
+              $sum: 1
+            }
+          }
+        }
+      
+    ])
+
+    res.status(200).json({
+      success: true,
+      data: result
+    });
+
+
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message
+    });
+    
+  }
+}
 
 const insertSampleProducts=async(req,res)=>{
     try {
@@ -55,5 +149,7 @@ const insertSampleProducts=async(req,res)=>{
 
 
 module.exports = {
-    insertSampleProducts
+    insertSampleProducts,
+    getAllProducts,
+    getProductAnalysis
 }
--- mongo-intermediate-pipeline/router/routes.js
+++ mongo-intermediate-pipeline/router/routes.js
@@ -1,8 +1,10 @@
 const express = require('express')
-const { insertSampleProducts } = require('../Controllers/Product-Controller')
+const { insertSampleProducts, getAllProducts, getProductAnalysis } = require('../Controllers/Product-Controller')
 
 const router = express.Router()
 
 router.post('/add' , insertSampleProducts)
+router.post('/getfilter' , getAllProducts)
+router.post('/getAnalysis' , getProductAnalysis)
 
 module.exports= router
